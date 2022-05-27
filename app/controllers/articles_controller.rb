6d3a69ca68572127a03e3ad398cd999792b216c0
iftikhar <iftikharmangiana.official@gmail.com>
2022-05-27 15:33:43 +0500

    Untill now all is done which is done in alpha-blog repository go and check, bcz i have changed rails version and copied all code of alpha-blog to this version of rails, check alpha-blog repo

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,36 +1,41 @@
 class ArticlesController < ApplicationController
-  def index
-    @articles = Article.all
-  end
 
+  
   def show
     @article = Article.find(params[:id])
+  end 
+    
+  def index
+    @articles = Article.all
   end
 
   def new
     @article = Article.new
   end
 
-  def create
-    @article = Article.new(art_params)
-    if @article.save
-      redirect_to @article
-    else
-      render "new"
-    end
-  end
-
   def edit
     @article = Article.find(params[:id])
   end
 
-  def update
-    @article = Article.find(params[:id])
-    if @article.update(art_params)
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :body))
+    if @article.save
+      flash[:notice]="Article Created Successfully"
       redirect_to @article
     else
-      render "edit"
+      render 'new', status: :unprocessable_entity
     end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :body))
+      flash[:notice] = "Article Updated Successfully"
+      redirect_to @article
+    else
+      render "new", status: :unprocessable_entity
+    end
+
   end
 
   def destroy
@@ -39,9 +44,4 @@ class ArticlesController < ApplicationController
     redirect_to articles_path
   end
 
-  private
-    
-    def art_params
-      params.require(:article).permit(:title, :body)
-    end
 end
