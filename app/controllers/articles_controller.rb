0b395a3903fcb7eac3772de987c567d76f90f3f2
iftikhar <iftikharmangiana.official@gmail.com>
2022-05-27 15:06:32 +0500

    Create resouceful routes,Create Articles Controller with index, show, new, create, edit, update, destroy actions, and art_params method in it, and create their respective  views templates, Create article model and migrate it

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,47 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(art_params)
+    if @article.save
+      redirect_to @article
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(art_params)
+      redirect_to @article
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+    
+    def art_params
+      params.require(:article).permit(:title, :body)
+    end
+end
